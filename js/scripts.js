0c2718da7f68786e63ccdb1729b3b3f5f364e78c
rizeup88 <marinersrock88@yahoo.com>
2019-08-09 14:39:19 -0700

    This commit will load a quiz that you can use to see which Code Lyoko character you are.

---
+++ js/scripts.js
@@ -0,0 +1,49 @@
+$(document).ready(function() {
+  $("form#choices").submit(function(event) {
+    var gender = $("select#gender").val();
+    var question1 = $("select#question1").val();
+    var question2 = $("select#question2").val();
+    var question3 = $("select#question3").val();
+    var question4 = $("select#question4").val();
+    var question5 = $("select#question5").val();
+    var question6 = $("select#question6").val();
+    var question7 = $("select#question7").val();
+
+    if (gender) {
+      var character = gender;
+      if (gender === 'female' && (question1 === 'extro') && (question2 === 'fan') && (question3 === 'run') && (question4 === 'hide') && (question5 === 'live') && (question6 === 'english') && (question7 === 'not')) {
+        character = ('Yumi Ishiyama');
+        image = ('#yumi');
+      } else if (gender === 'male' && (question1 === 'switch') && (question2 === 'sword') && (question3 === 'run') && (question4 === 'hide') && (question5 === 'half') && (question6 === 'physical') && (question7 === 'masc')) {
+        character = ('Ulrich Stern');
+        image = ('#ulrich');
+      } else if (gender === 'male' && (question1 === 'extro') && (question2 === 'arrow') && (question3 === 'excuse') && (question4 === 'flirt') && (question5 === 'dog') && (question6 === 'love') && (question7 === 'masc')) {
+        character = ('Odd Della Robbia');
+        image = ('#odd');
+      } else if (gender === 'male' && (question1 === 'intro') && (question2 === 'keyboard') && (question3 === 'distract') && (question4 === 'sneak') && (question5 === 'close') && (question6 === 'science') && (question7 === 'masc')) {
+        character = ('Jeremie Belpois');
+        image = ('#jeremie');
+      } else if (gender === 'female' && (question1 === 'intro') && (question2 === 'beam') && (question3 === 'excuse') && (question4 === 'sneak') && (question5 === 'none') && (question6 === 'math') && (question7 === 'fem')) {
+        character = ('Aelita Schaeffer');
+        image = ('#aelita');
+      } else if (gender === 'male' && (question1 === 'extro') && (question2 === 'blade') && (question3 === 'distract') && (question4 === 'flirt') && (question5 === 'trouble') && (question6 === 'love') && (question7 === 'masc')) {
+        character = ('William Dunbar');
+        image = ('#william');
+      } else if (gender === 'female' && (question1 === 'extro') && (question2 === 'voice') && (question3 === 'submit') && (question4 === 'flirt') && (question5 === 'spoiled') && (question6 === 'love') && (question7 === 'fem')) {
+        character = ('Sissi Delmas');
+        image = ('#sissi');
+      } else if (gender === 'female' && (question1 === 'extro') && (question2 === 'skateboard') && (question3 === 'run') && (question4 === 'sneak') && (question5 === 'trouble') && (question6 === 'art') && (question7 === 'not')) {
+        character = ('Samantha Knight');
+        image = ('#samantha');
+      }
+
+      $("#result").empty().append(character);
+      $("#character").show();
+      $(image).show();
+    } else {
+      alert('Please finish providing your answers.');
+    }
+
+    event.preventDefault();
+  });
+});
