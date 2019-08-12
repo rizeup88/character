cf41a25c6cabb904e478cf5119c435d489f3b567
rizeup88 <marinersrock88@yahoo.com>
2019-08-12 10:07:46 -0700

    This commit will include updated script to insure results are displayed with images for characters.

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,13 @@ $(document).ready(function() {
 
     if (gender) {
       var character = gender;
-      if (gender === 'female' && (question1 === 'extro') && (question2 === 'fan') && (question3 === 'run') && (question4 === 'hide') && (question5 === 'live') && (question6 === 'english') && (question7 === 'not')) {
+      if (gender === 'female' && (question1 === 'extro') && (question2 === 'fan') && (question3 === 'run') && ((question4 === 'hide') || (question4  === 'unsure')) && (question5 === 'live') && (question6 === 'english') && (question7 === 'not')) {
         character = ('Yumi Ishiyama');
         image = ('#yumi');
-      } else if (gender === 'male' && (question1 === 'switch') && (question2 === 'sword') && (question3 === 'run') && (question4 === 'hide') && (question5 === 'half') && (question6 === 'physical') && (question7 === 'masc')) {
+      } else if (gender === 'male' && (question1 === 'switch') && (question2 === 'sword') && (question3 === 'run') && ((question4 === 'hide') || (question4  === 'unsure')) && (question5 === 'half') && (question6 === 'physical') && (question7 === 'masc')) {
         character = ('Ulrich Stern');
         image = ('#ulrich');
-      } else if (gender === 'male' && (question1 === 'extro') && (question2 === 'arrow') && (question3 === 'excuse') && (question4 === 'flirt') && (question5 === 'dog') && (question6 === 'love') && (question7 === 'masc')) {
+      } else if (gender === 'male' && (question1 === 'extro') && (question2 === 'arrow') && ((question3 === 'excuse') || (question3 === 'distract')) && (question4 === 'flirt') && (question5 === 'dog') && (question6 === 'love') && (question7 === 'masc')) {
         character = ('Odd Della Robbia');
         image = ('#odd');
       } else if (gender === 'male' && (question1 === 'intro') && (question2 === 'keyboard') && (question3 === 'distract') && (question4 === 'sneak') && (question5 === 'close') && (question6 === 'science') && (question7 === 'masc')) {
@@ -29,10 +29,10 @@ $(document).ready(function() {
       } else if (gender === 'male' && (question1 === 'extro') && (question2 === 'blade') && (question3 === 'distract') && (question4 === 'flirt') && (question5 === 'trouble') && (question6 === 'love') && (question7 === 'masc')) {
         character = ('William Dunbar');
         image = ('#william');
-      } else if (gender === 'female' && (question1 === 'extro') && (question2 === 'voice') && (question3 === 'submit') && (question4 === 'flirt') && (question5 === 'spoiled') && (question6 === 'love') && (question7 === 'fem')) {
+      } else if (gender === 'female' && (question1 === 'extro') && (question2 === 'voice') && ((question3 === 'submit') || (question3 === 'excuse')) && (question4 === 'flirt') && (question5 === 'spoiled') && (question6 === 'love') && (question7 === 'fem')) {
         character = ('Sissi Delmas');
         image = ('#sissi');
-      } else if (gender === 'female' && (question1 === 'extro') && (question2 === 'skateboard') && (question3 === 'run') && (question4 === 'sneak') && (question5 === 'trouble') && (question6 === 'art') && (question7 === 'not')) {
+      } else if (gender === 'female' && (question1 === 'extro') && (question2 === 'skateboard') && (question3 === 'run') && (question4 === 'sneak') && (question5 === 'trouble') && (question6 === 'art') && (question7 === 'masc')) {
         character = ('Samantha Knight');
         image = ('#samantha');
       }
